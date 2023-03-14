7b9b565f61049bf446a653b88e88ccabfe163f5e
KimYongJae <yjkim1@jastech.co.kr>
2023-03-14 16:53:26 +0900

    폴더 이름 변경

---
+++ src/Jastech.Framework.Comm/TcpIpInfo.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Comm
+{
+    public class TcpIpInfo
+    {
+        #region 생성자 
+        public TcpIpInfo()
+        {
+            IpAddress = "127.0.0.1";
+            PortNo = 4000;
+        }
+
+        public TcpIpInfo(string ipAddress, int portNo)
+        {
+            this.IpAddress = ipAddress;
+            this.PortNo = portNo;
+        }
+        #endregion
+
+
+        #region 속성 
+        [JsonProperty]
+        public string IpAddress { get; set; }
+
+        [JsonProperty]
+        public int PortNo { get; set; }
+        #endregion
+
+
+        #region 메서드 
+        public TcpIpInfo Clone()
+        {
+            TcpIpInfo tcpIpInfo = new TcpIpInfo();
+            tcpIpInfo.IpAddress = this.IpAddress;
+            tcpIpInfo.PortNo = this.PortNo;
+            return tcpIpInfo;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", IpAddress, PortNo);
+        }
+        #endregion
+    }
+}
---
+++ src/Jastech.Framework.Config/ConfigSet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Jastech.Framework.Config.Config;
+
+namespace Jastech.Framework.Config
+{
+    public class ConfigSet
+    {
+
+        #region 속성
+        public PathConfig Path { get; private set; } = new PathConfig();
+
+        OperationConfig Operation { get; set; } = new OperationConfig();
+        #endregion
+
+        #region 이벤트
+        public event ConfigCreatedEventHandler<PathConfig> PathConfigCreated;
+
+        public event ConfigCreatedEventHandler<OperationConfig> OperationConfigCreated;
+        #endregion
+
+        #region 메서드
+
+        public void Initialize()
+        {
+            string curDir = Environment.CurrentDirectory;
+            string configPath = $"{curDir}\\..\\Config";
+
+            Path.Load(configPath, PathConfigCreated);
+            Operation.Load(configPath, OperationConfigCreated);
+        }
+
+        public void Save()
+        {
+            string curDir = Environment.CurrentDirectory;
+            string configPath = $"{curDir}\\..\\Config";
+
+            Path.Save(configPath);
+            Operation.Save(configPath);
+        }
+            
+        public bool Load()
+        {
+            string curDir = Environment.CurrentDirectory;
+            string configPath = $"{curDir}\\..\\Config";
+            bool success = Operation.Load<OperationConfig>(configPath);
+            success &= Path.Load<PathConfig>(configPath);
+
+            return success;
+        }
+        #endregion
+
+    }
+}
