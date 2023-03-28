f4d36b7d1c19983b1b672f4259046917d28e88ea
Park Junghwan <parkjh9010@gmail.com>
2023-03-29 08:47:55 +0900

    Add Version cs

---
+++ src/Jastech.Framework.Imaging.VisionPro/VisionAlgorithms/CogCaliper.cs
@@ -0,0 +1,48 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.Caliper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Imaging.VisionPro.VisionAlgorithms
+{
+    public class CogCaliper : CogVision
+    {
+        #region 필드
+        public CogCaliperTool CaliperTool { get; set; }
+        #endregion
+
+        #region 속성
+        #endregion
+
+        #region 이벤트
+        #endregion
+
+        #region 델리게이트
+        #endregion
+
+        #region 생성자
+        #endregion
+
+        #region 메서드
+        public CogCaliperTool SetRegion(CogRectangleAffine roi)
+        {
+            CogRectangleAffine rect = new CogRectangleAffine(roi);
+            rect.Color = CogColorConstants.Green;
+            rect.LineStyle = CogGraphicLineStyleConstants.Dot;
+
+            CaliperTool.Region = new CogRectangleAffine(rect);
+            CaliperTool.CurrentRecordEnable = CogCaliperCurrentRecordConstants.InputImage | CogCaliperCurrentRecordConstants.Region;
+
+            return CaliperTool;
+        }
+
+        public ICogRegion GetRegion()
+        {
+            return CaliperTool.Region;
+        }
+        #endregion
+    }
+}
--- src/Jastech.Framework.Imaging.VisionPro/VisionAlgorithms/Parameters/CogCaliperParam.cs
+++ src/Jastech.Framework.Imaging.VisionPro/VisionAlgorithms/Parameters/CogCaliperParam.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace Jastech.Framework.Imaging.VisionPro.VisionAlgorithms.Parameters
     public class CogCaliperParam
     {
         [JsonProperty]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty]
+        public int LeadCount { get; set; } = 0;
 
         [JsonIgnore]
         public CogCaliperTool CaliperTool { get; set; } = new CogCaliperTool();
@@ -20,9 +24,37 @@ namespace Jastech.Framework.Imaging.VisionPro.VisionAlgorithms.Parameters
         {
             CogCaliperParam param = new CogCaliperParam();
             param.Name = Name;
-            param.MatchingTool = new CogPMAlignTool(MatchingTool);
+            param.LeadCount = LeadCount;
+            param.CaliperTool = new CogCaliperTool(CaliperTool);
 
             return param;
+        }
+
+        public void SaveTool(string dirPath)
+        {
+            if (Directory.Exists(dirPath) == false)
+                Directory.CreateDirectory(dirPath);
+
+            string fileName = string.Format(@"{0}.vpp", Name);
+            string path = Path.Combine(dirPath, fileName);
+
+            CogFileHelper.SaveTool<CogCaliperTool>(path, CaliperTool);
+        }
+
+        public void LoadTool(string dirPath)
+        {
+            string fileName = string.Format("{0}.vpp", Name);
+            string path = Path.Combine(dirPath, fileName);
+
+            if (File.Exists(path))
+                CaliperTool = CogFileHelper.LoadTool(path) as CogCaliperTool;
+            else
+                SaveTool(dirPath);
+        }
+
+        public void Dispose()
+        {
+            CaliperTool.Dispose();
         }
     }
 }
