80fc60db8576fc0d375973950204644ac71ec7df
Park Junghwan <parkjh9010@gmail.com>
2023-03-30 16:54:08 +0900

    1. Add MotionCommandControl 2. Add MotionFunctionControl

---
+++ src/Jastech.Framework.Winform/Controls/MotionFunctionControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jastech.Framework.Winform.Controls
+{
+    public partial class MotionFunctionControl : UserControl
+    {
+        #region 필드
+        #endregion
+
+        #region 속성
+        #endregion
+
+        #region 이벤트
+        #endregion
+
+        #region 델리게이트
+        #endregion
+
+        #region 생성자
+        public MotionFunctionControl()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 메서드
+        #endregion
+    }
+}
