72fabaf72034d628c24dc2c0bfc046f4a9aa56e4
Park Junghwan <parkjh9010@gmail.com>
2023-04-03 15:23:14 +0900

    Add TeachingPosition.cs

---
+++ src/Jastech.Framework.Structure/TeachingPosition.cs
@@ -0,0 +1,51 @@
+﻿using Jastech.Framework.Device.Motions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Structure
+{
+
+    #region 필드
+    #endregion
+
+    #region 속성
+    #endregion
+
+    #region 이벤트
+    #endregion
+
+    #region 델리게이트
+    #endregion
+
+    #region 생성자
+    #endregion
+
+    #region 메서드
+    #endregion
+
+    public class TeachingPosition
+    {
+        [JsonProperty]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty]
+        public string Description { get; set; } = string.Empty;
+        
+        [JsonProperty]
+        public double TargetPosition { get; set; } = 0.0;
+
+        [JsonProperty]
+        public AxisMovingParam MovingParam { get; set; } = new AxisMovingParam();
+
+        public TeachingPosition(string name, AxisMovingParam param, string description)
+        {
+            Name = name;
+            MovingParam = param;
+            Description = description;
+        }
+    }
+}
--- src/Jastech.Framework.Winform/Forms/EditModelForm.Designer.cs
+++ src/Jastech.Framework.Winform/Forms/EditModelForm.Designer.cs
@@ -303,8 +303,8 @@
             // 
             // EditModelForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(544, 543);
             this.ControlBox = false;
