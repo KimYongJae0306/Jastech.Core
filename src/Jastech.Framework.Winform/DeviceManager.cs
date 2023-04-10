46f2e1d169e56f14c50fdc0d264ec8db41839fb5
KimYongJae <yjkim1@jastech.co.kr>
2023-04-10 17:17:27 +0900

    LightCtrlHandler 추가

--- src/Jastech.Framework.Winform/DeviceManager.cs
+++ src/Jastech.Framework.Winform/DeviceManager.cs
@@ -1,5 +1,6 @@
 ﻿using Jastech.Framework.Config;
 using Jastech.Framework.Device.Cameras;
+using Jastech.Framework.Device.LightCtrls;
 using Jastech.Framework.Device.Motions;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,8 @@ namespace Jastech.Framework.Winform
 
         public MotionHandler MotionHandler { get; private set; }
 
+        public LightCtrlHandler LightCtrlHandler { get; private set; }
+
         private ConfigSet ConfigSet { get; set; }
         #endregion
 
@@ -36,6 +39,7 @@ namespace Jastech.Framework.Winform
         {
             CameraHandler = new CameraHandler();
             MotionHandler = new MotionHandler();
+            LightCtrlHandler = new LightCtrlHandler();
         }
         #endregion
 
@@ -57,6 +61,7 @@ namespace Jastech.Framework.Winform
             var devices = ConfigSet.Machine.Devices;
             var cameras = devices.Where((d) => d is Camera).Cast<Camera>();
             var motions = devices.Where((d) => d is Motion).Cast<Motion>();
+            var lightCtrls = devices.Where((d) => d is LightCtrl).Cast<LightCtrl>();
 
             foreach (var camera in cameras)
             {
@@ -66,32 +71,26 @@ namespace Jastech.Framework.Winform
             {
                 MotionHandler.Add(motion);
             }
-            
+            foreach (var lightCtrl in lightCtrls)
+            {
+                LightCtrlHandler.Add(lightCtrl);
+            }
+
             bool cameraSuccess = CameraHandler.Initialize();
             Initialized?.Invoke(typeof(Camera), cameraSuccess);
 
             bool motionSuccess = MotionHandler.Initialize();
             Initialized?.Invoke(typeof(Motion), cameraSuccess);
+
+            bool lightCtrlSuccess = LightCtrlHandler.Initialize();
+            Initialized?.Invoke(typeof(LightCtrl), lightCtrlSuccess);
         }
 
         public void Release()
         {
             CameraHandler.Release();
             MotionHandler.Release();
-        }
-
-        //public Motion GetMotion()
-        //{
-        //    var motion = DeviceManager.Instance().MotionHandler.First();
-
-        //    return motion;
-        //}
-
-        public Camera GetCamera()
-        {
-            var camera = DeviceManager.Instance().CameraHandler.First();
-
-            return camera;
+            LightCtrlHandler.Release();
         }
         #endregion
     }
