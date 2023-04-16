1801238261283d2a71406d871b1bc3549b06b460
Park Junghwan <parkjh9010@gmail.com>
2023-04-17 07:40:57 +0900

    Add AkkonROI.cs

---
+++ src/Jastech.Framework.Macron.Akkon/Parameters/AkkonROI.cs
@@ -0,0 +1,42 @@
+﻿using Jastech.Framework.Util.Helper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Macron.Akkon.Parameters
+{
+    public class AkkonROI
+    {
+        [JsonProperty]
+        public double CornerOriginX { get; set; } = 0.0;          // 3251
+
+        [JsonProperty]
+        public double CornerOriginY { get; set; } = 0.1;          // 837
+
+        [JsonProperty]
+        public double CornerXX { get; set; } = 0.2;         // 3283
+
+        [JsonProperty]
+        public double CornerXY { get; set; } = 0.3;         // 837
+
+        [JsonProperty]
+        public double CornerOppositeX { get; set; } = 0.4;  // 3530
+
+        [JsonProperty]
+        public double CornerOppositeY { get; set; } = 0.5;   // 2435
+
+        [JsonProperty]
+        public double CornerYX { get; set; } = 0.6;         // 3498
+
+        [JsonProperty]
+        public double CornerYY { get; set; } = 0.7;         // 2435
+
+        public AkkonROI DeepCopy()
+        {
+            return JsonConvertHelper.DeepCopy(this) as AkkonROI;
+        }
+    }
+}
