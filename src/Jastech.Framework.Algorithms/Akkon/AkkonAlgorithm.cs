ed7777595bfbfb68524c0d461d69d99710955c85
KimYongJae <yjkim1@jastech.co.kr>
2023-05-22 14:31:46 +0900

    akkon 테스트 중

--- src/Jastech.Framework.Algorithms/Akkon/AkkonAlgorithm.cs
+++ src/Jastech.Framework.Algorithms/Akkon/AkkonAlgorithm.cs
@@ -2,10 +2,12 @@
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Jastech.Framework.Algorithms.Akkon.Parameters;
+using Jastech.Framework.Imaging.Helper;
 using Jastech.Framework.Imaging.Ipp;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -15,168 +17,249 @@ namespace Jastech.Framework.Algorithms.Akkon
 {
     public partial class AkkonAlgorithm
     {
-        public void Run(byte[] sourceData, int width, int height, AkkonParameters parameters)
+        public void Test()
         {
-            int overLap = 10;
+         
+        }
+        public void Run(IntPtr srcPoint, int width, int height, AkkonParameters parameters)
+        {
             // 기존 marcon Filter2 =>(sigma : 2, gusWidth : 8, logWidth : 16, scaleFactor : 1.3)
             AkkonFilterParam filterParam = GenerateFilter(2.0, 8, 16, 1.3);
 
-            // EnhanceY
-            IntPtr src16Ptr = IntPtr.Zero;
-            IntPtr dst16Ptr = IntPtr.Zero;
-            int src16step = width * sizeof(short);
-            int dst16step = width * sizeof(short);
-
-            try
+            unsafe
             {
-                src16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out src16step); // 16비트 메모리 할당
-                dst16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out dst16step); // 16비트 메모리 할당
+                IntPtr src16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out int src16step); // 16비트 메모리 할당
+                IntPtr dst16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out int dst16step); // 16비트 메모리 할당
 
-                short[] src16_Data = new short[width * height];
-                short[] dst16_Data = new short[width * height];
-                Marshal.Copy(src16Ptr, src16_Data, 0, width * height);
-                Marshal.Copy(dst16Ptr, dst16_Data, 0, width * height);
+                int result = IPPWrapper.ippiScale_8u16s_C1R(srcPoint, width, src16Ptr, src16step, new IppiSize(width, height));
 
-                IPPWrapper.ippiScale_8u16s_C1R(sourceData, width, src16_Data, src16step, width, height, 0.0, -128);
+                short[] t = new short[width * height];
+                Marshal.Copy(src16Ptr, t, 0, t.Length);
+                byte[] rawData = new byte[t.Length * sizeof(short)];
+                Buffer.BlockCopy(t, 0, rawData, 0, rawData.Length);
+
+                string filePath = @"D:\123.raw";
+                File.WriteAllBytes(filePath, rawData);
+
+
+                // ppDst 배열 설정
+                IntPtr[] ppDst = new IntPtr[height];
+                for (int i = 0; i < height; i++)
+                {
+                    ppDst[i] = srcPoint + i * width * sizeof(short);
+                }
+                short[] kernel = new short[] { -1, 0, 1 };
+                IntPtr kernelPtr = Marshal.AllocHGlobal(kernel.Length * sizeof(short));
+
+                // short[] 배열의 데이터를 IntPtr로 복사
+                Marshal.Copy(kernel, 0, kernelPtr, kernel.Length);
+                int kernelSize = kernel.Length;
+                int xAnchor = kernelSize / 2;
+                int borderType = 0;  
+                short borderValue = 0;
+                IppiSize roiSize = new IppiSize(width, height);
+
+                try
+                {
+                    //int g123 = IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R(src16Ptr, width, ppDst, roiSize, kernelPtr, kernelSize, xAnchor, 1, borderValue, 0, IntPtr.Zero);
+                }
+                catch (Exception err)
+                {
+
+                    throw;
+                }
+                
             }
-            finally
-            {
-                if (src16Ptr != IntPtr.Zero)
-                    IPPWrapper.ippiFree(src16Ptr);
-                if (dst16Ptr != IntPtr.Zero)
-                    IPPWrapper.ippiFree(dst16Ptr);
-            }
+
         }
-
         //public void Run(byte[] sourceData, int width, int height, AkkonParameters parameters)
         //{
-        //    int overLap = 10;
         //    // 기존 marcon Filter2 =>(sigma : 2, gusWidth : 8, logWidth : 16, scaleFactor : 1.3)
         //    AkkonFilterParam filterParam = GenerateFilter(2.0, 8, 16, 1.3);
 
         //    // EnhanceY
-        //    IntPtr src16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out int src16step); // 16비트 메모리 할당
-        //    IntPtr dst16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out int dst16step); // 16비트 메모리 할당
+        //    IntPtr src16Ptr = IntPtr.Zero;
+        //    IntPtr dst16Ptr = IntPtr.Zero;
+        //    int src16dataSize = height * width * sizeof(short);
+        //    int src16step = width * sizeof(short);
+        //    int dst16step = width * sizeof(short);
 
-        //    short[] src16_Data = new short[src16step * height];
-        //    short[] dst16_Data = new short[dst16step * height];
-        //    Marshal.Copy(src16Ptr, src16_Data, 0, src16step * height);
-        //    Marshal.Copy(dst16Ptr, dst16_Data, 0, dst16step * height);
+        //    try
+        //    {
+        //        //src16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out src16step); // 16비트 메모리 할당
+        //        //dst16Ptr = IPPWrapper.ippiMalloc_16s_C1(width, height, out dst16step); // 16비트 메모리 할당
 
-        //    //IntPtr src8Ptr = IPPWrapper.ippiMalloc_8u_C1(width, height, out int src8step);
+        //        short[] src16_Data = new short[width * height];
 
-        //    IPPWrapper.ippiScale_8u16s_C1R(sourceData, width, src16_Data, src16step, width, height, 0.0, -128);
+        //        int result = IPPWrapper.ippiScale_8u16s_C1R(sourceData, width, src16_Data, src16step, new IppiSize(width, height));
+        //        if (result != 0)
+        //            return;
+
+        //        result = IPPWrapper.ippiAddC_16s_C1IRSfs(-128, src16_Data, src16step, new IppiSize(width, height), 0);
+        //        if (result != 0)
+        //            return;
+        //        //Test();
+        //        SepConv16s_rowfirst(src16_Data, src16step, new IppiSize(width, height), filterParam);
+        //        //SepConv16s_rowfirst(src16Ptr, src16step,ref dst16Ptr, dst16step, new IppiSize(width, height), filterParam);
 
 
+        //        //short[] dst16_Data = new short[width * height];
+        //        //Marshal.Copy(dst16Ptr, dst16_Data, 0, width * height);
 
-        //    IPPWrapper.ippiFree(src16Ptr);
-        //    IPPWrapper.ippiFree(dst16Ptr);
+        //        //IntPtr dst8Ptr = IPPWrapper.ippsMalloc_8u(width * height);
+        //        //byte[] dst8Array = new byte[width * height];
+        //        //Marshal.Copy(dst8Ptr, dst8Array, 0, dst8Array.Length);
 
-        //    //IPPWrapper.ippiScale_8u16s_C1R(sourceData, src8step, src16Ptr,)
-        //    //int dst16Size = width * height;
-        //    //short[] dst16Data = new short[dst16Size];
-        //    //Marshal.Copy(dst16Data, 0, dst16Ptr, dst16Data.Length); // dst16Ptr 에 데이터 Copy
-        //    //IppiSize size = new IppiSize(width, height);
-        //    ////IPPWrapper.ippiScale_8u16s_C1R(sourceData, srcStep, dst16Data, dstStep, width, height, 0.0, -128); // 8Bit->16Bit로 변경
-        //    //IPPWrapper.ippiScale_8u16s_C1R(sourcePtr, srcStep, dst16Ptr, dstStep, new IppiSize(width, height), 0.0, -128); // 8Bit->16Bit로 변경
+        //        //IPPWrapper.ippiScale_16s8u_C1R(src16_Data, src16step, dst8Array, 0, new IppiSize(width, height), 1.0, 128);
 
-        //    #region SepConv16s_rowfirst
+        //        //Mat mat = MatHelper.ByteArrayToMat(dst8Array, width, height, 1);
+        //        //mat.Save(@"D:\123.bmp");
 
-        //    #region calc...
-        //    int sizerow = 0;
-        //    int sizecol = 0;
-        //    int i = 0;
-        //    int maxKernelSize = (filterParam.GusSize > filterParam.LogSize) ? filterParam.GusSize : filterParam.LogSize;
-
-        //    int nc = filterParam.LogSize;
-        //    int nr = filterParam.GusSize;
-
-        //    // compute the kernel semisizes
-        //    int ncss = nc >> 1;
-        //    int nrss = nr >> 1;
-
-        //    // compute the kernel offsets (0 -> odd, 1 -> even)
-        //    int co = 1 - (nc % 2);
-        //    int ro = 1 - (nr % 2);
-
-        //    IppiSize tempSize = new IppiSize();
-        //    tempSize.Width = width;
-        //    tempSize.Height = height + nc + 1;
-
-        //    int tempStep = 0;
-        //    IntPtr ptemp = IPPWrapper.ippiMalloc_16s_C1(tempSize.Width, tempSize.Height, out tempStep);
-        //    #endregion
-        //    int pilelRowSize = 0;
-        //    int pilelColumSize = 0;
-        //    int status = IPPWrapper.ippiFilterRowBorderPipelineGetBufferSize_16s_C1R(tempSize, nr, out pilelRowSize);
-        //    status = IPPWrapper.ippiFilterColumnPipelineGetBufferSize_16s_C1R(tempSize, nc, out pilelColumSize);
-
-        //    nrss -= ro;
-        //    ncss -= co;
-
-    
-
-        //    IntPtr pSrc = Marshal.AllocHGlobal(height + nc + 1);
-        //    IntPtr nDst = Marshal.AllocHGlobal(height);
-
-        //    IppiSize sourceSize = new IppiSize();
-        //    sourceSize.Width = width;
-        //    sourceSize.Height = height;
-
-        //    //IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R()
-        //    //IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R(sourceData, srcStep, destData, dstStep, sourceSize, IntPtr.Zero, Inptr.ze)
-        //    #endregion
-        //    //SepConv16s_rowfirst(sourceData, filterParam, new Size(width, height));
-
-        //    //SepConv16s_rowfirst(filterParam, )
-
-        //    IPPWrapper.ippiFree(src16Ptr);
-        //    IPPWrapper.ippiFree(dst16Ptr);
+        //    }
+        //    finally
+        //    {
+        //        if (src16Ptr != IntPtr.Zero)
+        //            IPPWrapper.ippiFree(src16Ptr);
+        //        if (dst16Ptr != IntPtr.Zero)
+        //            IPPWrapper.ippiFree(dst16Ptr);
+        //    }
         //}
-
-        private void SepConv16s_rowfirst(byte[] sourceData, int sourceStep, ref byte[] destData, int destStep, AkkonFilterParam filterParam, Size imageSize)
+        private void SepConv16s_rowfirst(short[] src16DataArray, int src16Step, IppiSize orgSize, AkkonFilterParam filterParam)
         {
-            //int sizerow = 0;
-            //int sizecol = 0;
-            //int i = 0;
-            //int maxKernelSize = (filterParam.GusSize > filterParam.LogSize) ? filterParam.GusSize : filterParam.LogSize;
+            int sizerow = 0;
+            int sizecol = 0;
+            int i = 0;
+            int maxKernelSize = (filterParam.GusSize > filterParam.LogSize) ? filterParam.GusSize : filterParam.LogSize;
 
-            //int nc = filterParam.LogSize;
-            //int nr = filterParam.GusSize;
+            int nc = filterParam.LogSize;
+            int nr = filterParam.GusSize;
 
-            //// compute the kernel semisizes
-            //int ncss = nc >> 1;
-            //int nrss = nr >> 1;
+            // compute the kernel semisizes
+            int ncss = nc >> 1;
+            int nrss = nr >> 1;
 
-            //// compute the kernel offsets (0 -> odd, 1 -> even)
-            //int co = 1 - (nc % 2);
-            //int ro = 1 - (nr % 2);
+            // compute the kernel offsets (0 -> odd, 1 -> even)
+            int co = 1 - (nc % 2);
+            int ro = 1 - (nr % 2);
 
-            //IppiSize tempSize = new IppiSize();
-            //tempSize.Width = imageSize.Width;
-            //tempSize.Height = imageSize.Height + nc + 1;
+            IppiSize tempSize = new IppiSize();
+            tempSize.Width = orgSize.Width;
+            tempSize.Height = orgSize.Height + nc + 1;
+            IntPtr tempPtr = IPPWrapper.ippiMalloc_16s_C1(tempSize.Width, tempSize.Height, out int tempStep);
 
-            //int pilelRowSize = 0;
-            //int pilelColumSize = 0;
+            IntPtr ppSrc = IPPWrapper.ippsMalloc_8u((orgSize.Height + nc + 1) * 8); // sizeof(Ipp16s*) = 8
+            //IntPtr ppDst = IPPWrapper.ippsMalloc_8u(orgSize.Height * 8);
 
-            //int status = IPPWrapper.ippiFilterRowBorderPipelineGetBufferSize_16s_C1R(tempSize, nr, out pilelRowSize);
-            //status = IPPWrapper.ippiFilterColumnPipelineGetBufferSize_16s_C1R(tempSize, nc, out pilelColumSize);
+            int status = IPPWrapper.ippiFilterRowBorderPipelineGetBufferSize_16s_C1R(orgSize, nr, out int rowBufferSize);
+            status = IPPWrapper.ippiFilterColumnPipelineGetBufferSize_16s_C1R(orgSize, nc, out int colBufferSize);
 
-            //nrss -= ro;
-            //ncss -= co;
+            IntPtr pBufferRow = IPPWrapper.ippsMalloc_8u(rowBufferSize);
+            IntPtr pBufferCol = IPPWrapper.ippsMalloc_8u(colBufferSize);
 
-            //int tempStep = 0;
-            //IntPtr ptemp = IPPWrapper.ippiMalloc_16s_C1(imageSize.Width, imageSize.Height, out tempStep);
+            nrss -= ro;
+            ncss -= co;
 
-            //IntPtr pSrc = Marshal.AllocHGlobal(imageSize.Height + nc + 1);
-            //IntPtr nDst = Marshal.AllocHGlobal(imageSize.Height);
+            unsafe
+            {
+                IntPtr source = Marshal.AllocHGlobal(src16DataArray.Length * sizeof(short));
+                Marshal.Copy(src16DataArray, 0, source, src16DataArray.Length);
 
-            //IppiSize sourceSize = new IppiSize();
-            //sourceSize.Width = imageSize.Width;
-            //sourceSize.Height = imageSize.Height;
+                IntPtr[] ppDst = new IntPtr[orgSize.Height];
+
+                for (int h = 0; h < orgSize.Height; h++)
+                {
+                    ppDst[h] = source + h * orgSize.Width * sizeof(short);
+                }
+
+                IntPtr kernel = Marshal.AllocHGlobal(filterParam.GusKernel.Length * sizeof(short));
+                Marshal.Copy(filterParam.GusKernel, 0, kernel, filterParam.GusKernel.Length);
+                int kernelSize = filterParam.GusSize;
+                int xAnchor = kernelSize / 2;// nrss;
+                int scrStep = tempStep;
+                int divisor = (int)filterParam.GusDivisor;
+                int result = IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R(source, orgSize.Width * sizeof(short), 
+                    ppDst, orgSize, IntPtr.Zero, kernelSize, xAnchor, 0, 0, divisor, IntPtr.Zero);
+
+            }
 
 
-            //IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R(sourceData, sourceStep, destData, dstStep, sourceSize, IntPtr.Zero, Inptr.ze)
+
+
+
+
+
+            //int re = IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R(arrayPtr, tempStep, arrayPtr, tempStep, orgSize
+            //    , kernel, kernelSize, nrss, (int)filterParam.GusDivisor, 1, 0);
+
+        }
+
+        private void SepConv16s_rowfirst(IntPtr src16Ptr, int src16Step, ref IntPtr dst16Ptr, int dst16Step, IppiSize orgSize, AkkonFilterParam filterParam)
+        {
+            int sizerow = 0;
+            int sizecol = 0;
+            int i = 0;
+            int maxKernelSize = (filterParam.GusSize > filterParam.LogSize) ? filterParam.GusSize : filterParam.LogSize;
+
+            int nc = filterParam.LogSize;
+            int nr = filterParam.GusSize;
+
+            // compute the kernel semisizes
+            int ncss = nc >> 1;
+            int nrss = nr >> 1;
+
+            // compute the kernel offsets (0 -> odd, 1 -> even)
+            int co = 1 - (nc % 2);
+            int ro = 1 - (nr % 2);
+
+            IppiSize tempSize = new IppiSize();
+            tempSize.Width = orgSize.Width;
+            tempSize.Height = orgSize.Height + nc + 1;
+
+            IntPtr tempPtr = IPPWrapper.ippiMalloc_16s_C1(tempSize.Width, tempSize.Height, out int tempStep);
+
+            //IntPtr ppSrc = IPPWrapper.ippsMalloc_8u((orgSize.Height + nc + 1) * 8);// sizeof(Ipp16s*) = 8
+            //IntPtr ppDst = IPPWrapper.ippsMalloc_8u(orgSize.Height * 8); // sizeof(Ipp16s*) = 8
+
+            // size of temporary buffers
+            int status = IPPWrapper.ippiFilterRowBorderPipelineGetBufferSize_16s_C1R(orgSize, nr, out int rowBufferSize);
+            status = IPPWrapper.ippiFilterColumnPipelineGetBufferSize_16s_C1R(orgSize, nc, out int colBufferSize);
+
+            IntPtr pBufferRow = IPPWrapper.ippsMalloc_8u(rowBufferSize);
+            IntPtr pBufferCol = IPPWrapper.ippsMalloc_8u(colBufferSize);
+
+            nrss -= ro;
+            ncss -= co;
+
+            IntPtr[] ppDst = new IntPtr[orgSize.Height];
+            IntPtr[] ppSrc = new IntPtr[orgSize.Height + nc + 1];
+
+            for (int ii = 0, jj = ncss; ii < orgSize.Height; ++ii, ++jj)
+            {
+                ppDst[ii] = IntPtr.Add(tempPtr, jj * orgSize.Width * sizeof(short));
+                ppSrc[jj] = IntPtr.Add(tempPtr, jj * orgSize.Width * sizeof(short));
+
+                Console.WriteLine("ppSrc: {0}  ppDst: {1}", ppSrc[jj], ppDst[ii]);
+            }
+
+            for (int ii = 0, jj = orgSize.Height + ncss; ii < ncss; ii++, jj++)
+            {
+                ppSrc[ii] = ppSrc[ncss];
+                ppSrc[jj] = ppSrc[orgSize.Height + ncss - 1];
+            }
+
+            if (co != 0)
+            {
+                ppSrc[orgSize.Height + (ncss * 2)] = ppSrc[orgSize.Height + ncss - 1];
+            }
+
+            IntPtr pKernel = Marshal.AllocHGlobal(filterParam.GusKernel.Length * sizeof(short));
+            Marshal.Copy(filterParam.GusKernel, 0, pKernel, filterParam.GusKernel.Length);
+
+
+            //status = IPPWrapper.ippiFilterRowBorderPipeline_16s_C1R
+            //    (src16Ptr, src16Step, ppDst[0], tempStep, orgSize.Width * 2, pKernel, filterParam.GusSize, nrss, (int)filterParam.GusDivisor, 1, 0 
+            //    );
+            
         }
 
         private AkkonFilterParam GenerateFilter(double sigma, int gusWidth, int logWidth, double scaleFactor)
--- src/Jastech.Framework.Imaging.Ipp/IPPWrapper.cs
+++ src/Jastech.Framework.Imaging.Ipp/IPPWrapper.cs
@@ -10,29 +10,57 @@ namespace Jastech.Framework.Imaging.Ipp
 {
     public static class IPPWrapper
     {
-        [DllImport("ippi.dll")] // 16비트 부호 있는 정수 형식의 이미지 데이터를 위한 메모리를 할당하는 기능
+        [DllImport("ippi.dll", CallingConvention = CallingConvention.Cdecl)] // 16비트 부호 있는 정수 형식의 이미지 데이터를 위한 메모리를 할당하는 기능
         public static extern IntPtr ippiMalloc_16s_C1(int width, int height, out int step);
 
-        [DllImport("ippi.dll")] // 8비트 부호 없는 정수 형식의 이미지 데이터를 위한 메모리를 할당하는 기능
+        [DllImport("ippi.dll", CallingConvention = CallingConvention.Cdecl)]// 8비트 부호 없는 정수 형식의 이미지 데이터를 위한 메모리를 할당하는 기능
         public static extern IntPtr ippiMalloc_8u_C1(int width, int height, out int step);
 
-        [DllImport("ippi.dll")] // 8비트 부호 없는 정수 이미지를 16비트 부호 있는 정수 이미지로 스케일링하고 데이터 형식을 변환하는 기능
-        public static extern int ippiScale_8u16s_C1R(byte[] pSrc, int srcStep, short[] pDst, int dstStep,int roiWidth, int roiHeight, double scaleFactor, int shift);
+        [DllImport("ippi.dll", CallingConvention = CallingConvention.Cdecl)] // 8비트 부호 없는 정수 이미지를 16비트 부호 있는 정수 이미지로 스케일링하고 데이터 형식을 변환하는 기능
+        public static extern int ippiScale_8u16s_C1R(IntPtr pSrc, int srcStep, IntPtr pDst, int dstStep, IppiSize roiSize);
+        //public static extern int ippiScale_8u16s_C1R(byte[] pSrc, int srcStep, short[] pDst, int dstStep, int roiSize, double scaleFactor, int shift);
+
+        [DllImport("ippi.dll")] // 16비트 signed integer 형식의 1채널 이미지를 8비트 unsigned integer 형식으로 스케일링하는 함수
+        public static extern int ippiScale_16s8u_C1R(short[] pSrc, int srcStep, byte[] pDst, int dstStep, IppiSize roiSize, double scale, short shift);
 
         [DllImport("ippcv.dll")] // 이미지의 가로 방향(row)에 대해 경계(border) 처리를 포함한 필터링 연산을 수행할 때 필요한 버퍼의 크기를 반환
         public static extern int ippiFilterRowBorderPipelineGetBufferSize_16s_C1R(IppiSize roiSize, int kernelSize, out int bufferSize);
 
-        [DllImport("ippcv.dll")] //  이미지의 세로 방향(column)에 대해 적용되는 필터링 연산을 수행할 때 필요한 버퍼의 크기를 반환
+        [DllImport("ippcv.dll")] // 이미지의 세로 방향(column)에 대해 적용되는 필터링 연산을 수행할 때 필요한 버퍼의 크기를 반환
         public static extern int ippiFilterColumnPipelineGetBufferSize_16s_C1R(IppiSize roiSize, int kernelSize, out int bufferSize);
+
+        [DllImport("ippi.dll")] // 16비트 signed integer 형식의 1채널 이미지에 대해 상수 값을 더하는 함수
+        public static extern int ippiAddC_16s_C1IRSfs(short value, short[] pSrcDst, int srcDstStep, IppiSize roiSize, int scaleFactor);
+
+
 
 
         [DllImport("ippcv.dll")]
         public static extern int ippiFilterRowBorderPipelineInit_16s_C1R(short[] pKernel,int kernelSize,int anchor, IppiBorderType borderType,byte[] pBuffer);
 
-        [DllImport("ippcore.dll")] // 6비트 부호 있는 정수(16s) 형식의 메모리 버퍼를 동적으로 할당
+        [DllImport("ipps.dll")] // 8비트 unsigned integer 형식의 메모리를 할당
+        public static extern IntPtr ippsMalloc_8u(int length);
+
+        [DllImport("ippcore.dll")] // 16비트 부호 있는 정수(16s) 형식의 메모리 버퍼를 동적으로 할당
         public static extern IntPtr ippsMalloc_16s(int length);
 
+        [DllImport("ippcv.dll")]
+        private static extern int ippiFilterRowBorderPipeline_16s_C1R(
+        IntPtr pSrc, int srcStep, IntPtr pDst, int dstStep,
+        int dstRoiSize, IntPtr pKernel, int kernelSize,
+        int anchor, IntPtr pBorderValues);
 
+        [DllImport("ippcv.dll")] // 16비트 signed integer 형식의 1채널 이미지에 대해 가로 방향 필터링을 수행하는 함수
+        public static extern int ippiFilterRowBorderPipeline_16s_C1R(IntPtr pSrc, int srcStep, IntPtr[] ppDst,
+                                                                    IppiSize roiSize, IntPtr pKernel, int kernelSize, int xAnchor,
+                                                                    int borderType, short borderValue, int divisor, IntPtr pBuffer);
+        //public static extern int ippiFilterRowBorderPipeline_16s_C1R(IntPtr pSrc, int srcStep, IntPtr pDst, int dstStep, 
+        //                                                                IppiSize roiSize, IntPtr pKernel, int kernelSize,
+        //                                                                int anchor, int divisor, int borderType, int borderValue);
+
+
+        [DllImport("ippcv.dll")] // 16비트 signed integer 형식의 1채널 이미지에 대해 세로 방향 필터링을 수행하는 함수
+        public static extern int ippiFilterColumnPipeline_16s_C1R(IntPtr pSrc, int srcStep, IntPtr pDst, int dstStep, IppiSize roiSize, IntPtr pSpec, IntPtr pBuffer, int bufferSize);
 
 
         [DllImport("ippcore.dll")] // 16비트 부호 있는 정수(16s) 형식의 이미지 컬럼에 대해 파이프라인 형태로 필터링을 수행
@@ -41,8 +69,64 @@ namespace Jastech.Framework.Imaging.Ipp
         [DllImport("ippi.dll")]
         public static extern void ippiFree(IntPtr ptr);
 
-        
+
+        public static short[] IppiMalloc_16s_C1(int width, int height, out int step)
+        {
+            IntPtr ptr = ippiMalloc_16s_C1(width, height,out step);
+            short[] data = new short[step * height];
+            Marshal.Copy(ptr, data, 0, step * height);
+
+            ippiFree(ptr);
+
+            return data;
+        }
+
+        public static byte[] IppsMalloc_8u(int length)
+        {
+            IntPtr ptr = ippsMalloc_8u(length);
+
+            byte[] data = new byte[length];
+            Marshal.Copy(ptr, data, 0, length);
+
+            ippiFree(ptr);
+
+            return data;
+        }
+
+        public static int FilterRowBorderPipeline(short[] src, int srcStep, short[] dst, int dstStep,
+       int dstRoiSize, short[] kernel, int kernelSize, int anchor, int[] borderValues)
+        {
+            // Pin the managed arrays in memory
+            GCHandle srcHandle = GCHandle.Alloc(src, GCHandleType.Pinned);
+            GCHandle dstHandle = GCHandle.Alloc(dst, GCHandleType.Pinned);
+            GCHandle kernelHandle = GCHandle.Alloc(kernel, GCHandleType.Pinned);
+            GCHandle borderValuesHandle = GCHandle.Alloc(borderValues, GCHandleType.Pinned);
+
+            try
+            {
+                // Get the IntPtr pointers to the pinned arrays
+                IntPtr pSrc = srcHandle.AddrOfPinnedObject();
+                IntPtr pDst = dstHandle.AddrOfPinnedObject();
+                IntPtr pKernel = kernelHandle.AddrOfPinnedObject();
+                IntPtr pBorderValues = borderValuesHandle.AddrOfPinnedObject();
+
+                // Call the IPP function
+                int result = ippiFilterRowBorderPipeline_16s_C1R(
+                    pSrc, srcStep, pDst, dstStep, dstRoiSize, pKernel, kernelSize, anchor, pBorderValues);
+
+                return result;
+            }
+            finally
+            {
+                // Release the pinned arrays
+                srcHandle.Free();
+                dstHandle.Free();
+                kernelHandle.Free();
+                borderValuesHandle.Free();
+            }
+        }
     }
+    [StructLayout(LayoutKind.Sequential)]
     public struct IppiSize
     {
         public int Width;  // 이미지의 너비
