03fbcbe4ba79f8f63726c1e123ebf23501b2e210
KimYongJae <yjkim1@jastech.co.kr>
2023-07-18 18:20:20 +0900

    파일 누락

---
+++ src/Jastech.Framework.Util/Native/Win32APIWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Util.Native
+{
+    public static class Win32APIWrapper
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool SetSystemTime(ref SystemTime systemTime);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SystemTime
+    {
+        public ushort Year;
+        public ushort Month;
+        public ushort DayOfWeek;
+        public ushort Day;
+        public ushort Hour;
+        public ushort Minute;
+        public ushort Second;
+        public ushort Milliseconds;
+    }
+}
