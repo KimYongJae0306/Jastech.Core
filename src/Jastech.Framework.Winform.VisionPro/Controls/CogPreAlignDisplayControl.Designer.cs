4dfa63778583701387e7ffadf83825b6bdac44cc
Park Junghwan <parkjh9010@gmail.com>
2023-08-03 10:58:41 +0900

    modify prealign display ui

--- src/Jastech.Framework.Winform.VisionPro/Controls/CogPreAlignDisplayControl.Designer.cs
+++ src/Jastech.Framework.Winform.VisionPro/Controls/CogPreAlignDisplayControl.Designer.cs
@@ -30,51 +30,55 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CogPreAlignDisplayControl));
-            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpCogPreAlignDisplay = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpRight = new System.Windows.Forms.TableLayoutPanel();
             this.cogRightDisplay = new Cognex.VisionPro.CogRecordDisplay();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.tlpLeft = new System.Windows.Forms.TableLayoutPanel();
             this.cogLeftDisplay = new Cognex.VisionPro.CogRecordDisplay();
-            this.lblAlignViewer = new System.Windows.Forms.Label();
-            this.tableLayoutPanel3.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.pnlRight = new System.Windows.Forms.Panel();
+            this.tlpCogPreAlignDisplay.SuspendLayout();
+            this.tlpRight.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cogRightDisplay)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tlpLeft.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.cogLeftDisplay)).BeginInit();
+            this.pnlLeft.SuspendLayout();
+            this.pnlRight.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel3
+            // tlpCogPreAlignDisplay
             // 
-            this.tableLayoutPanel3.ColumnCount = 3;
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 3F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel2, 2, 0);
-            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel1, 0, 0);
-            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 1;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(405, 200);
-            this.tableLayoutPanel3.TabIndex = 2;
+            this.tlpCogPreAlignDisplay.ColumnCount = 2;
+            this.tlpCogPreAlignDisplay.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCogPreAlignDisplay.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCogPreAlignDisplay.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpCogPreAlignDisplay.Controls.Add(this.tlpRight, 1, 0);
+            this.tlpCogPreAlignDisplay.Controls.Add(this.tlpLeft, 0, 0);
+            this.tlpCogPreAlignDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpCogPreAlignDisplay.Location = new System.Drawing.Point(0, 0);
+            this.tlpCogPreAlignDisplay.Name = "tlpCogPreAlignDisplay";
+            this.tlpCogPreAlignDisplay.RowCount = 1;
+            this.tlpCogPreAlignDisplay.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpCogPreAlignDisplay.Size = new System.Drawing.Size(405, 200);
+            this.tlpCogPreAlignDisplay.TabIndex = 2;
             // 
-            // tableLayoutPanel2
+            // tlpRight
             // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.cogRightDisplay, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(204, 0);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(201, 200);
-            this.tableLayoutPanel2.TabIndex = 3;
+            this.tlpRight.ColumnCount = 1;
+            this.tlpRight.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpRight.Controls.Add(this.lblRight, 0, 0);
+            this.tlpRight.Controls.Add(this.pnlRight, 0, 1);
+            this.tlpRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpRight.Location = new System.Drawing.Point(202, 0);
+            this.tlpRight.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpRight.Name = "tlpRight";
+            this.tlpRight.RowCount = 2;
+            this.tlpRight.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tlpRight.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpRight.Size = new System.Drawing.Size(203, 200);
+            this.tlpRight.TabIndex = 3;
             // 
             // cogRightDisplay
             // 
@@ -86,44 +90,44 @@
             this.cogRightDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cogRightDisplay.DoubleTapZoomCycleLength = 2;
             this.cogRightDisplay.DoubleTapZoomSensitivity = 2.5D;
-            this.cogRightDisplay.Location = new System.Drawing.Point(0, 40);
+            this.cogRightDisplay.Location = new System.Drawing.Point(0, 0);
             this.cogRightDisplay.Margin = new System.Windows.Forms.Padding(0);
             this.cogRightDisplay.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
             this.cogRightDisplay.MouseWheelSensitivity = 1D;
             this.cogRightDisplay.Name = "cogRightDisplay";
             this.cogRightDisplay.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogRightDisplay.OcxState")));
-            this.cogRightDisplay.Size = new System.Drawing.Size(201, 160);
+            this.cogRightDisplay.Size = new System.Drawing.Size(201, 158);
             this.cogRightDisplay.TabIndex = 2;
             // 
-            // label1
+            // lblRight
             // 
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 40);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "RIGHT";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
+            this.lblRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblRight.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
+            this.lblRight.ForeColor = System.Drawing.Color.White;
+            this.lblRight.Location = new System.Drawing.Point(0, 0);
+            this.lblRight.Margin = new System.Windows.Forms.Padding(0);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(203, 40);
+            this.lblRight.TabIndex = 2;
+            this.lblRight.Text = "RIGHT";
+            this.lblRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // tableLayoutPanel1
+            // tlpLeft
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.cogLeftDisplay, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.lblAlignViewer, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(201, 200);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.tlpLeft.ColumnCount = 1;
+            this.tlpLeft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLeft.Controls.Add(this.lblLeft, 0, 0);
+            this.tlpLeft.Controls.Add(this.pnlLeft, 0, 1);
+            this.tlpLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpLeft.Location = new System.Drawing.Point(0, 0);
+            this.tlpLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpLeft.Name = "tlpLeft";
+            this.tlpLeft.RowCount = 2;
+            this.tlpLeft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tlpLeft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpLeft.Size = new System.Drawing.Size(202, 200);
+            this.tlpLeft.TabIndex = 2;
             // 
             // cogLeftDisplay
             // 
@@ -135,56 +139,82 @@
             this.cogLeftDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cogLeftDisplay.DoubleTapZoomCycleLength = 2;
             this.cogLeftDisplay.DoubleTapZoomSensitivity = 2.5D;
-            this.cogLeftDisplay.Location = new System.Drawing.Point(0, 40);
+            this.cogLeftDisplay.Location = new System.Drawing.Point(0, 0);
             this.cogLeftDisplay.Margin = new System.Windows.Forms.Padding(0);
             this.cogLeftDisplay.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
             this.cogLeftDisplay.MouseWheelSensitivity = 1D;
             this.cogLeftDisplay.Name = "cogLeftDisplay";
             this.cogLeftDisplay.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogLeftDisplay.OcxState")));
-            this.cogLeftDisplay.Size = new System.Drawing.Size(201, 160);
+            this.cogLeftDisplay.Size = new System.Drawing.Size(200, 158);
             this.cogLeftDisplay.TabIndex = 3;
             // 
-            // lblAlignViewer
+            // lblLeft
             // 
-            this.lblAlignViewer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
-            this.lblAlignViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblAlignViewer.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
-            this.lblAlignViewer.ForeColor = System.Drawing.Color.White;
-            this.lblAlignViewer.Location = new System.Drawing.Point(0, 0);
-            this.lblAlignViewer.Margin = new System.Windows.Forms.Padding(0);
-            this.lblAlignViewer.Name = "lblAlignViewer";
-            this.lblAlignViewer.Size = new System.Drawing.Size(201, 40);
-            this.lblAlignViewer.TabIndex = 2;
-            this.lblAlignViewer.Text = "LEFT";
-            this.lblAlignViewer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
+            this.lblLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblLeft.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
+            this.lblLeft.ForeColor = System.Drawing.Color.White;
+            this.lblLeft.Location = new System.Drawing.Point(0, 0);
+            this.lblLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(202, 40);
+            this.lblLeft.TabIndex = 2;
+            this.lblLeft.Text = "LEFT";
+            this.lblLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLeft.Controls.Add(this.cogLeftDisplay);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 40);
+            this.pnlLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(202, 160);
+            this.pnlLeft.TabIndex = 4;
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlRight.Controls.Add(this.cogRightDisplay);
+            this.pnlRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlRight.Location = new System.Drawing.Point(0, 40);
+            this.pnlRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(203, 160);
+            this.pnlRight.TabIndex = 4;
             // 
             // CogPreAlignDisplayControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
-            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tlpCogPreAlignDisplay);
             this.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "CogPreAlignDisplayControl";
             this.Size = new System.Drawing.Size(405, 200);
-            this.tableLayoutPanel3.ResumeLayout(false);
-            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tlpCogPreAlignDisplay.ResumeLayout(false);
+            this.tlpRight.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.cogRightDisplay)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tlpLeft.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.cogLeftDisplay)).EndInit();
+            this.pnlLeft.ResumeLayout(false);
+            this.pnlRight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tlpCogPreAlignDisplay;
+        private System.Windows.Forms.TableLayoutPanel tlpRight;
         private Cognex.VisionPro.CogRecordDisplay cogRightDisplay;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.TableLayoutPanel tlpLeft;
         private Cognex.VisionPro.CogRecordDisplay cogLeftDisplay;
-        private System.Windows.Forms.Label lblAlignViewer;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Panel pnlRight;
+        private System.Windows.Forms.Panel pnlLeft;
     }
 }
