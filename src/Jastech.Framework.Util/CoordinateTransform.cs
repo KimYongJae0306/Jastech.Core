e935bf1b878b3fcc813deefabe1fb2f8df3170c0
KimYongJae <yjkim1@jastech.co.kr>
2023-08-09 09:21:19 +0000

    test

--- src/Jastech.Framework.Util/CoordinateTransform.cs
+++ src/Jastech.Framework.Util/CoordinateTransform.cs
@@ -1,4 +1,5 @@
 ﻿using Jastech.Framework.Util.Helper;
+using System;
 using System.Drawing;
 
 namespace Jastech.Framework.Util
@@ -103,6 +104,13 @@ namespace Jastech.Framework.Util
 
             var targetCenterPoint = TargetData.GetCenterPoint();
             var referenceCenterPoint = ReferenceData.GetCenterPoint();
+
+            Console.WriteLine("diffAngle : " + diffAngle.ToString());
+            Console.WriteLine("offsetPoint : " + offsetPoint.ToString());
+            Console.WriteLine("inputPoint : " + inputPoint.ToString());
+            Console.WriteLine("targetCenterPoint : " + targetCenterPoint.ToString());
+            Console.WriteLine();
+            Console.WriteLine();
 
             return MathHelper.GetCoordinate(targetCenterPoint, diffAngle, offsetPoint, inputPoint);
         }
