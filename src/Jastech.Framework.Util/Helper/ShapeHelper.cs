e1a42cf911ffc416085e75657e22457f442bf5f3
Park Junghwan <parkjh9010@gmail.com>
2024-01-18 14:26:03 +0900

    Add ShapeHelper

---
+++ src/Jastech.Framework.Util/Helper/ShapeHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jastech.Framework.Util.Helper
+{
+    public static class ShapeHelper
+    {
+        public static Rectangle GetVaildRectangle(Rectangle inputRect, int imageWidht, int imageHeight)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            if (inputRect != null)
+            {
+                if (inputRect.X < 0)
+                    rectangle.X = 0;
+
+                if (inputRect.Y < 0)
+                    rectangle.Y = 0;
+
+                if (inputRect.Right > imageWidht)
+                    rectangle.Width = imageWidht - inputRect.X;
+
+                if (inputRect.Bottom > imageHeight)
+                    rectangle.Height = imageHeight - inputRect.Y;
+            }
+
+            return rectangle;
+        }
+    }
+}
