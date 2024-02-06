e55a6a5c4fde63d80c04b7d566208cc5dc81a83e
KimYongJae <yjkim1@jastech.co.kr>
2024-02-06 11:51:43 +0900

    add recursive control list

---
+++ src/Jastech.Framework.Winform/Helper/ControlHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jastech.Framework.Winform.Helper
+{
+    public static class ControlHelper
+    {
+        public static Control[] GetAllControlsUsingRecursive(Control containerControl)
+        {
+            List<Control> controlList = new List<Control>();
+
+            foreach (Control control in containerControl.Controls)
+            {
+                controlList.Add(control);
+
+                if (control.Controls.Count > 0)
+                    controlList.AddRange(GetAllControlsUsingRecursive(control));
+            }
+
+            return controlList.ToArray();
+        }
+    }
+}
